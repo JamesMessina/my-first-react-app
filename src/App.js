d2b76c1e33272154f467e29d2fe54538a4d515b9
James Messina <jmessina17@gmail.com>
2021-05-31 16:11:16 -0500

    updated app.js and made class with compnenet extension. added multiple methods and divs/js code in jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
+import React, { Component } from 'react';  
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      isClicked: false,
+      inputValue: "",
+      todoList: []
+    }
+  }
+
+  handleChange = (event) =>{
+    this.setState({inputValue: event.target.value})
+  }
+
+  handleSubmit = (event) =>{
+    event.preventDefault()
+    this.setState({todoList: [...this.state.todoList, this.state.inputValue]})
+    this.setState({inputValue: ""})
+  }
+
+
+  render(){
+    return (
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} className="App-logo" alt="logo"/>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.inputValue} onChange={this.handleChange}></input>
+          <button type="submit">Submit</button>
+        </form>
+        <ol>{this.state.todoList.map((todo, index) => {
+          return <li key={index}>{todo}</li>
+        })}</ol>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
           Learn React
         </a>
       </header>
     </div>
   );
+}
 }
 
 export default App;
