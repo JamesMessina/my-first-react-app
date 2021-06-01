eb94be9c9e883bbfd71a3b38cea4d47481101d45
James Messina <jmessina17@gmail.com>
2021-06-01 17:51:36 -0500

    updated app.js file to include submit and delete functions

--- src/App.js
+++ src/App.js
@@ -13,14 +13,29 @@ class App extends Component {
     }
   }
 
+  handleClick = () =>{
+    if(this.state.isClicked === false){
+      this.setState({isClicked: true});
+    }else{
+      this.setState({isClicked: false}); 
+    }
+  }
+
   handleChange = (event) =>{
     this.setState({inputValue: event.target.value})
   }
 
   handleSubmit = (event) =>{
-    event.preventDefault()
+    event.preventDefault();
     this.setState({todoList: [...this.state.todoList, this.state.inputValue]})
     this.setState({inputValue: ""})
+  }
+
+  handleDelete = (index) =>{
+    console.log(this.state.todoList[index])
+    this.state.todoList.splice(index, 1); 
+    this.setState({todoList: [...this.state.todoList]})
+    console.log(this.state.todoList); 
   }
 
 
@@ -29,13 +44,19 @@ class App extends Component {
       <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo"/>
+        <button onClick={this.handleClick}>Click Me!</button>
+        <p>{this.state.isClicked ? "true" : "false"}</p>
         <form onSubmit={this.handleSubmit}>
           <input type="text" value={this.state.inputValue} onChange={this.handleChange}></input>
           <button type="submit">Submit</button>
         </form>
         <ol>{this.state.todoList.map((todo, index) => {
-          return <li key={index}>{todo}</li>
-        })}</ol>
+          return <li key={index}>
+                    {todo}
+                    <button onClick={()=> this.handleDelete(index)}>Delete todo</button>
+                 </li>
+                })}
+        </ol>
         <a
           className="App-link"
           href="https://reactjs.org"
